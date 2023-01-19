866fae48fbb4a54672cadd3eaa47ff22953bc0f9
Elie Mbuyi Kayembe <mbuyieliek@gmail.com>
2023-01-19 16:11:04 +0100

    second commit

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const form = document.querySelector("form");
 const inputName = document.querySelector("form > #name");
 const inputPhoto = document.querySelector("form > #photo");
 const inputPost = document.querySelector("form > #post");
-console.log(form,inputName,inputPhoto,inputPost);
+const divParagraphe = document.querySelectorAll(".paragrapheRouge");
+//console.log(divparagraphe);
 var time = new Date();
 
 console.log(time.toLocaleString());
@@ -16,13 +17,29 @@ form.addEventListener("submit", e => {
     const valueInputName = inputName.value;
     const valueInputPhoto = inputPhoto.value;
     const valueInputPost = inputPost.value;
-    
-    inputName.value = '';
-    inputPhoto.value = '';
-    inputPost.value = '';
+    if (valueInputName === "" && valueInputPost === "") {
 
-    addPost(valueInputName,valueInputPhoto,valueInputPost);
-    displayPost();
+        divParagraphe.forEach(function (pText) {
+            pText.textContent = "Veuillez remplir le champs";
+            pText.classList.add("paragrapheRougeP");
+        });
+
+
+    } else {
+        inputName.value = '';
+        inputPhoto.value = '';
+        inputPost.value = '';
+
+        divParagraphe.forEach(function (pText) {
+            pText.textContent = " ";
+            pText.classList.remove("paragrapheRougeP");
+        });
+
+        addPost(valueInputName, valueInputPhoto, valueInputPost);
+        displayPost();
+    }
+
+
 })
 
 // photo loading
@@ -33,33 +50,73 @@ const posts = [
     {
         nom: "texte",
         photo: "images/images.jpeg",
-        postText:"lorem fsdsff fsfsf fsfs",
+        postText: "lorem fsdsff fsfsf fsfs",
         //date : Date.now()
     },
-    {
-        nom: "texte",
-        photo: "images/images.jpeg",
-        postText:"lorem fsdsff fsfsf fsfs",
-        //date : Date.now()
-    },
+
 ];
 
 // show the post
-const displayPost = ()=>{
-    const postsNode = posts.map((post,index) => {
-        return createPostElement(post,index)
+const displayPost = () => {
+    const postsNode = posts.map((post, index) => {
+        return createPostElement(post, index)
     })
     listPost.innerHTML = '';
-    listPost.append(... postsNode);
-    
+    listPost.append(...postsNode);
+
 
 }
 // create a post
-const createPostElement = (post,index) =>{
+const createPostElement = (post, index) => {
     const div = document.createElement('div');
+    //const idListPost = document.createAttribute("id");
+    //idListPost.value = "listPost";
+
+    const divOnePost = document.createElement('div');
+    const onePostClass = document.createAttribute("class");
+    onePostClass.value = "onePost";
+
+    divOnePost.setAttributeNode(onePostClass);
+
+    const divHead = document.createElement('div');
+    const idDivHead = document.createAttribute("id");
+    idDivHead.value = "head";
+
+    divHead.setAttributeNode(idDivHead);
+
+    const img = document.createElement("img")
+    const imgSrc = document.createAttribute("src");
+
+    img.setAttributeNode(imgSrc)
+
+    imgSrc.value = "images/images.jpeg";
+    const h3 = document.createElement("h3");
+    const h3Text = document.createTextNode("${post.nom}");//erreur
+    //const h3Text = document.createTextNode("nom");//erreur
+    const pHead = document.createElement('p');
+    const pHeadText = document.createTextNode("1/19/2023");
+
+    const divContent = document.createElement('div');
+    const idContent = document.createAttribute("id");
+    idContent.value = "content";
+
+    divContent.setAttributeNode(idContent);
+
+    const pContent = document.createElement('p');
+    const pContentText = document.createTextNode("salutdd")
+    //pContent.nodeValue = "gdgdgdgd"; // arevoir 
+
+    div.appendChild(divOnePost);
+    divOnePost.append(divHead, divContent);
+    divHead.append(img, h3, pHead);
+    h3.appendChild(h3Text);
+    pHead.appendChild(pHeadText);
+    divContent.appendChild(pContentText);
+
+
     // div avec la classe onePost
 
-    div.innerHTML = `
+    /*div.innerHTML = `
     <div class="onePost">
         <div id="head">
             <img src="images/images.jpeg" >
@@ -71,12 +128,12 @@ const createPostElement = (post,index) =>{
             </div>
     </div>
     
-    `
+    `*/
     return div;
 }
 
 // add a post
-const addPost = (nom,photo,postText) =>{
+const addPost = (nom, photo, postText) => {
 
     posts.push({
         nom,
